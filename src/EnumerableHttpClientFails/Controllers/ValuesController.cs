d82a33d939ad67b85366c1b31f75ef15baf8abcf
Marco Braga <marco.braga@gmail.com>
2016-04-12 14:12:03 +0200

    Initial commit

---
+++ src/EnumerableHttpClientFails/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace EnumerableHttpClientFails.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+		[HttpGet("rangefails")]
+		public IActionResult TestArrayFails()
+		{
+			return Json(GetTestRange());
+		}
+
+		[HttpGet("rangetolist")]
+		public IActionResult TestArrayToList()
+		{
+			return Json(GetTestRange().ToList());
+		}
+
+		[HttpGet("rangecount")]
+		public IActionResult TestArrayCount()
+		{
+			var range = GetTestRange();
+			var count = range.Count();
+			return Json(range);
+		}
+
+
+		private IEnumerable<string> GetTestRange()
+		{
+			return Enumerable.Range(1, 1000).Select(n => "0123456789");
+		}
+	}
+}
---
+++ src/Tests/Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EnumerableHttpClientFails;
+using Microsoft.AspNet.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Tests
+{
+	// This project can output the Class library as a NuGet Package.
+	// To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+	public class Tests
+	{
+		private readonly TestServer _server;
+		private readonly HttpClient _client;
+
+
+		public Tests()
+		{
+			_server = new TestServer(TestServer.CreateBuilder().UseStartup<Startup>());
+			_client = _server.CreateClient();
+		}
+
+		[Fact]
+		public async Task GetRangeToList_Passes()
+		{
+			var getResponse = await _client.GetAsync("/api/values/rangetolist");
+			var fetchedData = await getResponse.Content.ReadAsStringAsync();
+
+			var results = JsonConvert.DeserializeObject<IEnumerable<string>>(fetchedData);
+
+			Assert.Equal(1000, results.Count());
+		}
+
+		[Fact]
+		public async Task GetRangeCount_Passes()
+		{
+			var getResponse = await _client.GetAsync("/api/values/rangecount");
+			var fetchedData = await getResponse.Content.ReadAsStringAsync();
+
+			var results = JsonConvert.DeserializeObject<IEnumerable<string>>(fetchedData);
+
+			Assert.Equal(1000, results.Count());
+		}
+
+		[Fact]
+		public async Task GetRangeFails_Fails()
+		{
+			var getResponse = await _client.GetAsync("/api/values/rangefails");
+			var fetchedData = await getResponse.Content.ReadAsStringAsync();
+
+			var results = JsonConvert.DeserializeObject<IEnumerable<string>>(fetchedData);
+
+			Assert.Equal(1000, results.Count());
+		}
+	}
+}
